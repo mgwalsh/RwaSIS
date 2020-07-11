ead8941175701deb6c9a8efc6646a9af2c7d2877
mgwalsh <markusgwalsh@gmail.com>
2020-07-11 14:58:09 +0300

    Rwanda wet chemistry soil data setup

---
+++ RW_wetchem_data.R
@@ -0,0 +1,55 @@
+# Rwanda wet chemistry soil data setup
+# M. Walsh, July 2020
+
+# Required packages
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(raster)
+  require(leaflet)
+  require(htmlwidgets)
+})
+
+# Data downloads -----------------------------------------------------------
+# Create a data folder in  your current working directory
+dir.create("wetchem", showWarnings=F)
+setwd("./wetchem")
+
+# download soil data
+download("https://osf.io/djcfz?raw=1", "RW_wetchem.zip", mode = "wb")
+unzip("RW_wetchem.zip", overwrite = T)
+prof <- read.table("Profiles.csv", header = T, sep = ",")
+samp <- read.table("Samples.csv", header = T, sep = ",")
+geos <- merge(prof, samp, by="pid")
+
+# download raster stack
+download("https://osf.io/hp6v7?raw=1", "RW_250m_2020.zip", mode = "wb")
+unzip("RW_250m_2020.zip", overwrite = T)
+glist <- list.files(pattern="tif", full.names = T)
+grids <- stack(glist)
+
+# Data setup --------------------------------------------------------------
+# project GeoSurvey coords to grid CRS
+geos.proj <- as.data.frame(project(cbind(geos$lon, geos$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(geos.proj) <- c("x","y")
+geos <- cbind(geos, geos.proj)
+coordinates(geos) <- ~x+y
+projection(geos) <- projection(grids)
+
+# extract gridded variables at GeoSurvey locations
+geosgrid <- extract(grids, geos)
+gsdat <- as.data.frame(cbind(geos, geosgrid))
+
+# Write data frame --------------------------------------------------------
+dir.create("Results", showWarnings = F)
+write.csv(gsdat, "./Results/RW_soil_data.csv", row.names = F)
+
+# GeoSurvey map widgets ---------------------------------------------------
+# soil sampling locations
+w <- leaflet() %>%
+  setView(lng = mean(gsdat$lon), lat = mean(gsdat$lat), zoom = 9) %>%
+  addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
+  addCircleMarkers(gsdat$lon, gsdat$lat, clusterOptions = markerClusterOptions())
+w ## plot widget 
+saveWidget(w, 'RW_soil_sample.html', selfcontained = T) ## save widget
+
