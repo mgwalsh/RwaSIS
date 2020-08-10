e94204eb028aaf1405b8c48b1edbc3cfe1abd944
mgwalsh <markusgwalsh@gmail.com>
2020-08-11 01:42:14 +0300

    Spatially balanced sampling setup for soil and cropping system surveys of Rwanda

--- RW_MS_sample.R
+++ RW_MS_sample.R
@@ -18,8 +18,8 @@ dir.create("RW_MS_sample", showWarnings=F)
 setwd("./RW_MS_sample")
 
 # download GADM-L5 shapefile (courtesy: http://www.gadm.org)
-download("https://www.dropbox.com/s/8rbdzrheyi60ide/GADM.zip?raw=1", "GADM.zip", mode="wb")
-unzip("GADM.zip", overwrite=T)
+download("https://www.dropbox.com/s/fhusrzswk599crn/RWA_level5.zip?raw=1", "RWA_level5.zip", mode="wb")
+unzip("RWA_level5.zip", overwrite=T)
 shape <- shapefile("gadm36_RWA_5.shp")
 
 # download cropland mask
@@ -42,8 +42,8 @@ rmask <- index[which(index$index == 1),]
 
 # Geographically balanced sampling ----------------------------------------
 # set sampling parameters
-N <- nrow(rmask) ## population size (in 250 m pixels)
-n <- round(N/16,0) ## set sample size (number of sampling locations)
+N <- nrow(rmask) ## ROI size (in 250 m pixels)
+n <- round(N/16*0.15,0) ## set sample size (number of sampling locations)
 p <- rep(n/N,N)  ## inclusion probabilities
 
 # draw geographically balanced sample
@@ -51,17 +51,13 @@ set.seed(6405)                      ## sets repeatable randomization seed
 B <- cbind(p, rmask[,1], rmask[,2]) ## specifies balancing variables
 rsamp <- cube(p, B)                 ## samples from population
 
-# plot sample result
-# plot(roi, axes=F, legend=F)
-# points(rmask[rsamp,1], rmask[rsamp,2], pch=3, col="red", cex=0.3)
-
 # Write files -------------------------------------------------------------
 # extract sample coordinates
 x <- rmask[rsamp,1]
 y <- rmask[rsamp,2]
 xy <- data.frame(cbind(x,y))
 
-# attach GADM-L2 and above unit names from shape
+# attach GADM-L5 and above unit names from shape
 coordinates(xy) <- ~x+y
 crs(xy) <- "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"
 sloc <- spTransform(xy, CRS(proj4string(shape)))
@@ -69,14 +65,13 @@ gadm <- sloc %over% shape
 sloc <- as.data.frame(sloc)
 samp <- cbind(gadm[ ,c(4,6,8,10,12)], sloc)
 colnames(samp) <- c("region","district","L3","L4","L5","lon","lat")
-write.csv(samp, "RWA_GS_sample.csv", row.names = F)
+write.csv(samp, "RW_MS_samp15.csv", row.names = F)
 
 # Sampling map widget -----------------------------------------------------
 # render map
-w <- leaflet() %>% 
+w <- leaflet() %>%
+  setView(lng = mean(samp$lon), lat = mean(samp$lat), zoom = 9) %>%
   addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
   addCircleMarkers(samp$lon, samp$lat, clusterOptions = markerClusterOptions())
 w ## plot widget 
-
-# save widget
-saveWidget(w, 'RWA_GS_sample.html', selfcontained = T)
+saveWidget(w, 'RW_MS_sample.html', selfcontained = T) ## save widget
