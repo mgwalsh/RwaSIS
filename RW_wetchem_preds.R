61a71d74a5eb116273865bf5938e14d16e40da9f
mgwalsh <markusgwalsh@gmail.com>
2020-09-18 14:11:22 +0300

    Stacked spatial predictions of Rwanda soil properties

--- RW_wetchem_preds.R
+++ RW_wetchem_preds.R
@@ -2,7 +2,7 @@
 # M. Walsh, September 2020
 
 # Required packages
-# install.packages(c("devtools","caret","mgcv","MASS","randomForest","gbm","nnet","plyr","doParallel","dismo")), dependencies=T)
+# install.packages(c("devtools","caret","mgcv","MASS","randomForest","gbm","Cubist","plyr","doParallel","dismo")), dependencies=T)
 suppressPackageStartupMessages({
   require(devtools)
   require(caret)
@@ -10,10 +10,9 @@ suppressPackageStartupMessages({
   require(MASS)
   require(randomForest)
   require(gbm)
-  require(nnet)
+  require(Cubist)
   require(plyr)
   require(doParallel)
-  require(dismo)
 })
 
 # Data setup --------------------------------------------------------------
@@ -179,17 +178,18 @@ tc <- trainControl(method="repeatedcv", number=10, repeats=3, allowParallel = T)
 
 cu <- train(fcal, lcal, 
             method = "cubist", 
-            trControl = tc)
+            trControl = tc,
+            metric = "RMSE")
 
 print(cu)
+cu.pred <- predict(grids, cu) ## spatial predictions
 stopCluster(mc)
 fname <- paste("./Results/", labs, "_cu.rds", sep = "")
 saveRDS(cu, fname)
 
-
 # Model stacking setup ----------------------------------------------------
-preds <- stack(gm.pred, gl1.pred, gl2.pred, rf.pred, gb.pred, nn.pred)
-names(preds) <- c("gm","gl1","gl2","rf","gb","nn")
+preds <- stack(gm.pred, gl1.pred, gl2.pred, rf.pred, gb.pred, cu.pred)
+names(preds) <- c("gm","gl1","gl2","rf","gb","cu")
 plot(preds, axes = F)
 
 # extract model predictions
@@ -201,82 +201,33 @@ gspred <- as.data.frame(cbind(gs_val, gspred))
 # stacking model validation labels and features
 gs_val <- as.data.frame(gs_val)
 lval <- as.vector(t(gs_val[labs]))
-fval <- gspred[,62:67] ## subset validation features
+fval <- gspred[,78:83] ## subset validation features
 
 # Model stacking ----------------------------------------------------------
 # start doParallel to parallelize model fitting
 mc <- makeCluster(detectCores())
 registerDoParallel(mc)
 
-# control setup
-set.seed(1385321)
-tc <- trainControl(method = "cv", classProbs = T, 
-                   summaryFunction = twoClassSummary, allowParallel = T)
+# model setup
+set.seed(seed)
+tc <- trainControl(method="repeatedcv", number=10, repeats=3, allowParallel=T)
 
-# model training
-si <- train(fval, lval,
-            method = "glm",
-            family = "binomial",
-            metric = "ROC",
-            trControl = tc)
+st <- train(fval, lval,
+            method = "glmStepAIC",
+            trControl = tc,
+            metric = "RMSE")
 
-# model outputs & predictions
-summary(si)
-print(si)
-si.pred <- predict(preds, si, type = "prob") ## spatial predictions
-plot(si.pred, axes = F)
+print(st)
+summary(st)
+st.pred <- predict(preds, st) ## spatial predictions
 stopCluster(mc)
-fname <- paste("./Results/", labs, "_si.rds", sep = "")
-saveRDS(si, fname)
-
-# Receiver-operator characteristics ---------------------------------------
-cp_pre <- predict(si, fval, type="prob")
-cp_val <- cbind(lval, cp_pre)
-cpa <- subset(cp_val, cp_val=="A", select=c(A))
-cpb <- subset(cp_val, cp_val=="B", select=c(A))
-cp_eval <- evaluate(p=cpa[,1], a=cpb[,1]) ## calculate ROC's on test set
-plot(cp_eval, 'ROC') ## plot ROC curve
-
-# Generate feature mask ---------------------------------------------------
-t <- threshold(cp_eval) ## calculate thresholds based on ROC
-r <- matrix(c(0, t[,1], 0, t[,1], 1, 1), ncol=3, byrow = T) ## set threshold value <kappa>
-mask <- reclassify(si.pred, r) ## reclassify stacked predictions
-plot(mask, axes=F)
+plot(st.pred, axes=F)
+fname <- paste("./Results/", labs, "_st.rds", sep = "")
+saveRDS(st, fname)
 
 # Write prediction grids --------------------------------------------------
-gspreds <- stack(preds, si.pred, mask)
-names(gspreds) <- c("gm","gl1","gl2","rf","gb","nn","si","mk")
-fname <- paste("./Results/","OAF_", labs, "_preds_2020.tif", sep = "")
+gspreds <- stack(preds, st.pred)
+names(gspreds) <- c("gm","gl1","gl2","rf","gb","cu","st")
+fname <- paste("./Results/","RW_", labs, "_preds_2020.tif", sep = "")
 writeRaster(gspreds, filename=fname, datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
-
-# Site index prediction check ---------------------------------------------
-coordinates(gsdat) <- ~x+y
-projection(gsdat) <- projection(grids)
-gspre <- extract(gspreds, gsdat)
-gsout <- as.data.frame(cbind(gsdat, gspre))
-gsout$mzone <- as.factor(ifelse(gsout$mk == 1, "A", "B"))
-confusionMatrix(gsout$mzone, gsout$qy) ## overall prediction accuracy stats
-boxplot(yield~mzone, notch=T, xlab="Management zone", ylab="Measured yield (t/ha)",
-        cex.lab=1.3, gsout) ## yield differences between predicted site index zones
-
-# Maize yield potentials (t/ha) ------------------------------------------
-yld.lme <- lmer(log(yield)~factor(trt)*si+I(dap/50)*I(can/50)+(1|year)+(1|GID), data = gsout)
-summary(yld.lme) ## mixed model yield estimate results
-gsout$yldf <- exp(fitted(yld.lme, gsout))
-
-# Quantile regression (uncertainty) plot
-par(pty="s")
-par(mfrow=c(1,1), mar=c(5,5,1,1))
-plot(yield~yldf, xlab="Maize yield prediction (t/ha)", ylab="Measured yield (t/ha)", cex.lab=1.3, 
-     xlim=c(-1,15), ylim=c(-1,15), gsout)
-stQ <- rq(yield~yldf, tau=c(0.05,0.5,0.95), data=gsout)
-print(stQ)
-curve(stQ$coefficients[2]*x+stQ$coefficients[1], add=T, from=0, to=15, col="blue", lwd=2)
-curve(stQ$coefficients[4]*x+stQ$coefficients[3], add=T, from=0, to=15, col="red", lwd=2)
-curve(stQ$coefficients[6]*x+stQ$coefficients[5], add=T, from=0, to=15, col="blue", lwd=2)
-abline(c(0,1), col="grey", lwd=1)
-
-# Write output data frame -------------------------------------------------
-fname <- paste("./Results/","OAF_", labs, "_out.csv", sep = "")
-write.csv(gsout, fname, row.names = F)
 
