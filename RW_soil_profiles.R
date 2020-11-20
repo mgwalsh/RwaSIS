c4a1181d9ec0bb21d5a94b4a073190edf785b76e
mgwalsh <markusgwalsh@gmail.com>
2020-11-20 08:03:05 +0300

    Rwanda "CPR" soil profile locations (2018)

--- RW_soil_profiles.R
+++ RW_soil_profiles.R
@@ -1,4 +1,4 @@
-# Rwanda "CPR" soil profiles (~2018)
+# Rwanda "CPR" soil profile locations (2018)
 # Soil profile location data courtesy of Rwanda Agricultural Bureau (RAB) 
 # M. Walsh, November 2020
 
@@ -11,11 +11,11 @@ suppressPackageStartupMessages({
   require(htmlwidgets)
 })
 
-# Data setup --------------------------------------------------------------
 # create a data folder in your current working directory
 dir.create("RW_soil_profiles", showWarnings=F)
 setwd("./RW_soil_profiles")
 
+# Data downloads ----------------------------------------------------------
 # download CPR profile locations
 download.file("https://osf.io/y7bts?raw=1", "RW_soil_profiles.csv")
 sprof <- read.table("RW_soil_profiles.csv", header = T, sep = ",")
@@ -30,3 +30,24 @@ download.file("https://osf.io/bmysp?raw=1", "RW_CP_mask.zip")
 unzip("RW_CP_mask.zip", overwrite=T)
 glist <- list.files(pattern="tif", full.names=T)
 grids <- stack(glist)
+
+# Data setup --------------------------------------------------------------
+# attach GADM-L5 admin unit names from shape
+coordinates(sprof) <- ~lon+lat
+projection(sprof) <- projection(shape)
+gadm <- sprof %over% shape
+sprof <- as.data.frame(sprof)
+sprof <- cbind(gadm[ ,c(4,6,8,10,12)], sprof)
+# colnames(geos) <- c("region","district","sector","cell", "village","time","observer","id","lat","lon","BP","CP","TP","WP","bloc","cgrid")
+
+# project soil profile coords to grid CRS
+sprof.proj <- as.data.frame(project(cbind(sprof$lon, sprof$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(sprof.proj) <- c("x","y")
+sprof <- cbind(sprof, sprof.proj)
+coordinates(sprof) <- ~x+y
+projection(sprof) <- projection(grids)
+
+# extract gridded variables at soil profile locations
+sprofgrid <- extract(grids, sprof)
+spdat <- as.data.frame(cbind(sprof, sprofgrid))
+
